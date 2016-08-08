425048c2789a5da57b1534e7ee5e089fac27579e
mishalshah101 <mishalshah@plexusmd.com>
2016-08-08 15:26:38 +0530

    Bug fix

--- Employee.php
+++ Employee.php
@@ -4,13 +4,17 @@ $username = "team03";
 $password = "team123456";
 $dbname = "employeedb";
 
-$conn = mysqli_connect($servername,$username,$password,$dbname);
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-echo "Connected successfully";
+$link = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 echo $_POST["firstname"];
 echo $_POST["ID"];
