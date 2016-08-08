866f5a54b8099f541b9e77e9bae3840daa713085
nitesh singh <nitesh singh>
2016-08-08 15:19:21 +0530

    search

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
    $servername = "rds-server-team-03.ca5qg3gmjo0i.us-west-2.rds.amazonaws.com";
 $username = "team03";
 $password = "team123456";
-
+$con = mysqli_connect()
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
