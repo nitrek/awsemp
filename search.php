9ffdee61a6e7c7c6aab43d7923c77ed15096d347
nitesh singh <nitesh singh>
2016-08-08 15:38:16 +0530

    search

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 $servername = "rds-server-team-03.ca5qg3gmjo0i.us-west-2.rds.amazonaws.com";
 $username = "team03";
 $password = "team123456";
-$db = "employeedb"
+$db = "employeedb";
 // Check connection
 $conn = mysql_connect($servername,$username,$password,$db);
 
