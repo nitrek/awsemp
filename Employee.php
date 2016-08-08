5a5283835c83ed12ba7cf8b66e0a28aefb164fff
mishalshah101 <mishalshah@plexusmd.com>
2016-08-08 15:05:06 +0530

    Bug fix

--- Employee.php
+++ Employee.php
@@ -2,12 +2,21 @@
 $servername = "rds-server-team-03.ca5qg3gmjo0i.us-west-2.rds.amazonaws.com";
 $username = "team03";
 $password = "team123456";
-
+/*
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+*/
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+ or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
+
+//select a database to work with
+$selected = mysql_select_db("employeedb",$dbhandle) 
+  or die("Could not select examples");
 
 echo $_POST["firstname"];
 echo $_POST["ID"];
@@ -23,6 +32,9 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+//execute the SQL query and return records
+$result = mysql_query($sql);
+echo $result;
 //SELECTING DATA
 
 
