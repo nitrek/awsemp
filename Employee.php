e7c43de6f0305ed996d971dc51507ffdc5494ea3
mishalshah101 <mishalshah@plexusmd.com>
2016-08-08 15:40:21 +0530

    Bug fix

--- Employee.php
+++ Employee.php
@@ -4,30 +4,14 @@ $username = "team03";
 $password = "team123456";
 $dbname = "employeedb";
 
-$link = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+$con = mysqli_connect($servername,$username,$password,$dbname) or die("Some error occurred during connection " . mysqli_error($con));  
+$strSQL = "INSERT INTO tbl_EmployeeDetails(employee_name,employee_email_id,department ) VALUES ('".$_POST['firstname']."', '".$_POST['email']."', '".$_POST['dept']."')");
+$query = mysqli_query($con, $strSQL);
+while($result = mysqli_fetch_array($query))
+{
+  echo $result["username"]."";
 }
 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-echo $_POST["firstname"];
-echo $_POST["ID"];
-echo $_POST["email"];
-echo $_POST["dept"];
-
-
-//INSERTING DATA
-$sql = "INSERT INTO tbl_EmployeeDetails(employee_name,employee_email_id,department ) VALUES ($_POST["firstname"],$_POST["email"],$_POST["dept"]);";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+// Close the connection
+mysqli_close($con);
 ?>
