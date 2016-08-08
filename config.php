742350cb85ed5a76a2e03ec8b374fe137af84983
nitesh singh <nitesh singh>
2016-08-08 14:19:47 +0530

    db connect

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DATABASE_TYPE', 'mysql:');
+    define('SERVER', 'rds-server-team-03.ca5qg3gmjo0i.us-west-2.rds.amazonaws.com');
+    define('USERNAME', 'team03');
+    define('PASSWORD', 'team123456');
+    define('DATABASE', 'employeedb');
+?>
