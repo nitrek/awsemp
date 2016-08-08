2ff32c8e0c698c971968da2ea57333749b3db2ee
mishalshah101 <mishalshah@plexusmd.com>
2016-08-08 15:12:21 +0530

    Bug fix

--- Employee.php
+++ Employee.php
@@ -15,9 +15,8 @@ echo $_POST["email"];
 echo $_POST["dept"];
 
 mysql_select_db( 'employeedb' );
-
+/*
 //INSERTING DATA
-//SQL QUERY 1
 $sql = "INSERT INTO tbl_EmployeeDetails(employee_name,employee_email_id,department ) VALUES ($_POST["firstname"],$_POST["email"],$_POST["dept"]);";
 
 if ($conn->query($sql) === TRUE) {
@@ -25,6 +24,13 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+//SEARCHING DATA
+$sql = "SELECT * FROM tbl_EmployeeDetails WHERE emp_id = '$_POST["ID"]'";
 
-
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+*/
 ?>
